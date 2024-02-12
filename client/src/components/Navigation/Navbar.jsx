bccd24eae5fd346501cb251af5a3eec348089e3f
astrahalora <astrahalora@gmail.com>
2024-02-12 23:07:52 +0200

    Add more to nav

--- client/src/components/Navigation/Navbar.jsx
+++ client/src/components/Navigation/Navbar.jsx
@@ -1,13 +1,20 @@
 import { Outlet, NavLink, useNavigate } from "react-router-dom";
-import tazzaLogo from "../../img/tarantazza_logo.png";
+import { useState } from "react";
+import tazzaLogo from "../../img/tazza_logo.png";
+import menu from "../../img/menu.png";
+import close from "../../img/close.png";
 import { useWindowDimensions } from "../../js/useWindowDimensions";
+import "./Navbar.css";
 
 export default function Navbar() {
     const navigate = useNavigate();
-    const { width } = useWindowDimensions();
+    // const { width } = useWindowDimensions();
 
-    const wideNav = ``;
-    const hamburgerNav = ``;
+    // const wideNav = ``;
+    // const hamburgerNav = ``;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const handleOpenMenu = () => setIsOpen(prev => !prev);
 
     return (
         <>
@@ -29,7 +36,12 @@ export default function Navbar() {
                 <NavLink to="/" className="navbar-logo"> 
                     <img src={tazzaLogo} alt="A La Tarantazza Logo"/>
                 </NavLink>
+                <div className="menu-icon" onClick={handleOpenMenu}>
+                    <img src={isOpen ? menu : close} alt="Open or Close" />
+
+                </div>
                 <NavLink to="/" className="nav-link">About</NavLink>
+
             </nav>
             <Outlet />
         </>
