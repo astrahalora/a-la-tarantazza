f855f23138de13887bbc30ff2e106a457f3012cf
astrahalora <astrahalora@gmail.com>
2024-02-13 16:23:37 +0200

    Finish dropdown base

--- client/src/components/Navigation/Dropdown/Dropdown.jsx
+++ client/src/components/Navigation/Dropdown/Dropdown.jsx
@@ -7,9 +7,27 @@ import Drinks from "../../../pages/Menu/Drinks/Drinks";
 import "./Dropdown.css";
 
 export default function Dropdown () {
+    const [isOpen, setIsOpen] = useState(false);
+    const handleOpenMenu = () => setIsOpen(prev => !prev);
+
     return (
         <div>
             <h3>Dropdown</h3>
+            <ul onClick={handleOpenMenu}
+            className={isOpen ? "dropdown-menu clicked" : "dropdown-menu"}>
+                <li>
+                    <NavLink to="/pizza" onClick={() => setIsOpen(false)}>Pizza</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/desserts" onClick={() => setIsOpen(false)}>Desserts</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/salads" onClick={() => setIsOpen(false)}>Salads</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/drinks" onClick={() => setIsOpen(false)}>Drinks</NavLink>
+                </li>
+            </ul>
         </div>
     )
 }
