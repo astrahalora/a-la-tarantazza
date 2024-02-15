18b8913435942e7fad011c4d6b849d074efa3bdc
astrahalora <astrahalora@gmail.com>
2024-02-15 21:03:34 +0200

    Finish basic menu desriptions

--- client/src/pages/Menu/Menu.jsx
+++ client/src/pages/Menu/Menu.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import pizza from "../../img/pizza.webp";
+import desserts from "../../img/desserts.webp";
+import salads from "../../img/salads.webp";
+import drinks from "../../img/drinks.webp";
 import "./Menu.css";
 
 export default function Menu() {
@@ -11,17 +14,44 @@ export default function Menu() {
         <div className="main-menu">
             {/* <h2>{productList === null ? "Null" : "Products"}</h2> */}
             <div className="pizza-info infos" onClick={() => navigate("/pizza")}>
-                <img src={pizza} alt="Pizza"/>
-                <h2>Pizza</h2>
+                <img src={pizza} alt="Pizza" />
+                <div>
+                    <h2>Pizza</h2>
+                    <p> - expert hand-kneaded dough </p>
+                    <p> - gluten free alternatives </p>
+                    <p> - vegan alternatives </p>
+                    <p> - quality in every bite</p>
+                    <p> - wide range of toppings </p>
+                </div>
             </div>
             <div className="desserts-info infos" onClick={() => navigate("/desserts")}>
-                <h2>Desserts</h2>
+                <img src={desserts} alt="Pizza" />
+                <div>
+                    <h2>Desserts</h2>
+                    <p> - traditional oven-baked </p>
+                    <p> - locally sourced fruits </p>
+                    <p> - milk from casein A2 cows only </p>
+                    <p> - enriched with essential vitamins </p>
+                    <p> - team led by Reonaldo Di Alfonso (celebrity baker)</p>
+                </div>
             </div>
             <div className="salads-info infos" onClick={() => navigate("/salads")}>
-                <h2>Salads</h2>
+                <img src={salads} alt="Pizza" />
+                <div>
+                    <h2>Salads</h2>
+                    <p> - fresh in any season</p>
+                    <p> - for every 50 salads sold, we plant a tree </p>
+                    <p> - devoted to excellence, promoting healthier eating</p>
+                </div>
             </div>
             <div className="drinks-info infos" onClick={() => navigate("/drinks")}>
-                <h2>Drinks</h2>
+                <img src={drinks} alt="Pizza" />
+                <div>
+                    <h2>Drinks</h2>
+                    <p> - low alcoholic content </p>
+                    <p> - fresh and flavorful</p>
+                    <p> - happiness in a bottle</p>
+                </div>
             </div>
         </div>
     )
