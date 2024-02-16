4e4935ff5c80df522e23031a6aa37043e95b8a23
astrahalora <astrahalora@gmail.com>
2024-02-16 22:16:11 +0200

    Add banner imgs

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,7 +1,13 @@
+import "./About.css";
+import newProduct from "../../img/product_promotion.webp";
+import anniversary from "../../img/celebrate.webp";
+import delivery from "../../img/delivery.webp";
+
+
 export default function About() {
     return (
-        <div>
-            <h2>About time to write something here</h2>
+        <div className="about">
+            <img src={anniversary} alt="Anniversary voucher"/>
         </div>
     )
 }
