ed230b72ebb11bad21c3ed60a2e97a015372c02f
astrahalora <astrahalora@gmail.com>
2024-02-21 21:02:20 +0200

    Update if product is in cart

--- client/src/pages/ProductDetails/ProductDetails.jsx
+++ client/src/pages/ProductDetails/ProductDetails.jsx
@@ -1,11 +1,28 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { addProductToCart, removeProductFromCart } from "../../redux/cartSlice";
 import heartEmpty from "../../img/heart_empty.png";
 import heartFull from "../../img/heart_full.png";
 import "./ProductDetails.css";
 
 export default function ProductDetails() {
     const detailsState = useSelector(state => state.product.details);
+    const productsInCart = useSelector(state => state.cart.products);
+    const dispatch = useDispatch();
 
+    const addToCart = (product) => {
+        dispatch(addProductToCart(product));
+    }
+
+    const quantityInCart = () => {
+        const foundProduct = productsInCart.find(product => product._id === detailsState[0]._id);
+    
+        if (foundProduct && foundProduct.quantity !== undefined) {
+            return foundProduct.quantity;
+        }
+    
+        return null;
+    }
+    
     return (
         <div className="product-details-frame">
             <img src={heartEmpty} alt="Empty Heart" className="liked" />
@@ -41,8 +58,14 @@ export default function ProductDetails() {
                         <div className="interaction-btns">
                             <div className="add-or-remove-btns">
                                 <button type="button" className="decrement-btn">-</button>
-                                <p>0</p>
-                                <button type="button" className="increment-btn">+</button>
+                                <p>{quantityInCart() ? quantityInCart() : "0"}</p>
+                                <button 
+                                    type="button" 
+                                    className="increment-btn" 
+                                    onClick={() => addToCart(detailsState[0])}
+                                    disabled={detailsState[0].amount === 0}>
+                                    +
+                                </button>
                             </div>
                             <button type="button" className="favorite-btn base-btn">
                                 Add to Favorites
