42bf6a0901448806d8868b33f27fece0a9a8d50b
astrahalora <astrahalora@gmail.com>
2024-02-21 17:03:08 +0200

    Finish desserts page

--- client/src/pages/Menu/Desserts/Desserts.jsx
+++ client/src/pages/Menu/Desserts/Desserts.jsx
@@ -1,27 +1,69 @@
-import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setProduct } from "../../../redux/productDetailsSlice";
+import { filterByType, filterOutByString, filterByPhrase } from "../../../js/filtering";
+import { getAllergenList } from "../../../js/getAllergenList";
 import Loading from "../../Loading/Loading";
 import ErrorPage from "../../Error/ErrorPage";
-import { filterByType, filterOutByString, filterByPhrase } from "../../../js/filtering";
 import Product from "../../../components/Product/Product";
 import "../Products.css";
+import Filter from "../../../components/Filter/Filter";
 
 export default function Desserts() {
     const productsState = useSelector(state => state.productList);
-    const [dessert, setDessert] = useState([]);
+    const [data, setData] = useState([]);
+    const [filteredDesserts, setFilteredDesserts] = useState([]);
+    const [desserts, setDesserts] = useState([]);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const showProductDetails = (product) => {
+        dispatch(setProduct([product]));
+        navigate("/product-details");
+    }
 
     useEffect(() => {
-        setDessert(filterByType(productsState.products, "dessert"));
+        setData(filterByType(productsState.products, "dessert"));
+        setFilteredDesserts(filterByType(productsState.products, "dessert"));
+        setDesserts(filterByType(productsState.products, "dessert"));
     }, [productsState.products]);
 
     if (productsState.loading) return <Loading />;
     if (productsState.error) return <ErrorPage />;
 
+    const filterByAllergen = (input, select) => {
+        const optionName = select.current.value;
+        input.current.value = "";
+        setFilteredDesserts(data);
+        setDesserts(data);
+
+        if(optionName === "-- See All --") return;
+        setFilteredDesserts(prev => filterOutByString(prev, optionName));
+        setDesserts(prev => filterOutByString(prev, optionName));
+    }
+
+    const searchByName = (input) => {
+        const searchPhrase = input.current.value.toLowerCase();
+        setDesserts(filteredDesserts);
+        setDesserts(prev => filterByPhrase(prev, searchPhrase));
+    };
+
     return (
         <section className="product-list">
-            {dessert.map((product) => (
-                <Product key={product._id} product={[product]} />
+            <Filter 
+                allergens={getAllergenList(data)}
+                filter={filterByAllergen}
+                search={searchByName}
+                />
+            <div className="listed-products">
+            {desserts.map((product) => (
+                <Product
+                    key={product._id}
+                    product={[product]}
+                    onClick={() => showProductDetails(product)} />
             ))}
+            </div>
         </section>
     );
 }
