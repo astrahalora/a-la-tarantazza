b9b66febb966c15043a505455d50e2b863434bb6
astrahalora <astrahalora@gmail.com>
2024-02-23 17:50:19 +0200

    Add content to favs page

---
+++ client/src/components/Favorite/Favorite.jsx
@@ -0,0 +1,30 @@
+import "./Favorite.css";
+
+export default function Favorite( {favorite} ) {
+    return (
+        <div className="favorite">
+            <div className="favorite-1">
+                <h3>{favorite[0].name}</h3>
+                <img src={favorite[0].imageUrl} alt={favorite[0].name} className="fav-img"/>
+            </div>
+            <div className="favorite-2">
+                <div>
+                    <h4>Ingredients: </h4>
+                    <p>
+                        {favorite[0].ingredients.map(item => (
+                            <span key={item}>{item}, </span>
+                        ))}
+                    </p>
+                </div>
+                <div>
+                    <h4>Allergens: </h4>
+                    <p>
+                        {favorite[0].alergens.length > 0 ? favorite[0].alergens.map(item => (
+                            <span key={item}>{item}, </span>
+                        )) : "None"}
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/pages/Favorites/Favorites.jsx
+++ client/src/pages/Favorites/Favorites.jsx
@@ -1,9 +1,29 @@
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import Favorite from "../../components/Favorite/Favorite";
+import Loading from "../Loading/Loading";
+import ErrorPage from "../Error/ErrorPage";
 import "./Favorites.css";
 
 export default function Favorites() {
+    const favoriteState = useSelector(state => state.favoriteList);
+    const [favorites, setFavorites] = useState([]);
+
+    useEffect(() => {
+        setFavorites(favoriteState.favorites);
+    }, [favoriteState.favorites]);
+
+    if (favoriteState.loading) return <Loading />;
+    if (favoriteState.error) return <ErrorPage />;
+
     return (
-        <div>
+        <div className="favorites-menu">
             <h2>Favorites</h2>
+            <div className="favorites">
+                {favorites.map(favorite => (
+                    <Favorite key={favorite._id} favorite={[favorite]} />
+                ))}
+            </div>
         </div>
     )
 }
