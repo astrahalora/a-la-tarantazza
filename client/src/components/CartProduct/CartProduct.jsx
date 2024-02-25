37e01a0911d1a258c92071c2c00ad064bd731fe1
astrahalora <astrahalora@gmail.com>
2024-02-25 21:34:18 +0200

    Add cart bow to products

--- client/src/components/CartProduct/CartProduct.jsx
+++ client/src/components/CartProduct/CartProduct.jsx
@@ -1,29 +1,34 @@
+import cartBow from "../../img/bow_3.png";
 import "./CartProduct.css";
 
-export default function CartProduct( { product }) {
+export default function CartProduct({ product }) {
     const productObj = product[0];
 
     return (
         <div className="cart-product">
             <div className="cp-details-1">
                 <p>{productObj.name}</p>
-                <img src={productObj.imageUrl} alt={productObj.name} className="cp-img"/>
+                <img src={productObj.imageUrl} alt={productObj.name} className="cp-img" />
+            </div>
+            <div className="cart-bow-div">
+                <img src={cartBow} alt="Cart Product Bow"
+                className="cart-bow" />
             </div>
             <div className="cp-details-2">
                 <p>${productObj.price * productObj.quantity}</p>
                 <div className="cp-actions">
-                    <button 
-                    type="button"
+                    <button
+                        type="button"
                     >-</button>
                     <p>0</p>
-                    <button 
-                    type="button"
+                    <button
+                        type="button"
                     >+</button>
                 </div>
                 <div className="remove-btn-div">
-                    <button 
-                    type="button"
-                    className="base-btn">
+                    <button
+                        type="button"
+                        className="base-btn">
                         Remove
                     </button>
                 </div>
