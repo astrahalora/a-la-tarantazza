b4456af0e7204dff4df2ded04074a05c54059119
astrahalora <astrahalora@gmail.com>
2024-02-25 16:07:03 +0200

    Add base order component styles

--- client/src/components/OrderDetails/OrderDetails.jsx
+++ client/src/components/OrderDetails/OrderDetails.jsx
@@ -3,20 +3,30 @@ import "./OrderDetails.css";
 export default function OrderDetails() {
     return (
         <div className="order-details">
-                <div className="order-summary">
-                    <p>Products Cost: </p>
-                    <p>Shipping Cost: </p>
-                    <p>Total:</p>
-                    <button type="button">Order Now</button>
-                </div>
-                <div className="voucher">
-                    <p>Do you have a voucher?</p>
-                    {/* if no voucher applied */}
-                    <div className="no-voucher">
-                        <input type="text" placeholder="Code..." />
-                        <button type="apply-voucher">Add</button>
-                    </div>
+            <div className="order-summary">
+                <p>Products Cost: </p>
+                <p>Shipping Cost: </p>
+                <p>Total:</p>
+                <div className="order-btn-div">
+                    <button
+                        type="button"
+                        className="base-btn">
+                        Order Now
+                    </button>
                 </div>
             </div>
+            <div className="voucher">
+                <p>Do you have a voucher?</p>
+                {/* if no voucher applied */}
+                <div className="no-voucher">
+                    <input type="text" placeholder="Code..." />
+                    <button
+                        type="apply-voucher"
+                        className="base-btn">
+                        Add
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
