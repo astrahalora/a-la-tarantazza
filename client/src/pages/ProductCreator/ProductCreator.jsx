a38426c263ac2eaf62fe171326c58f8630cddd12
astrahalora <astrahalora@gmail.com>
2024-03-04 12:36:18 +0200

    Add post req in product creator

--- client/src/pages/ProductCreator/ProductCreator.jsx
+++ client/src/pages/ProductCreator/ProductCreator.jsx
@@ -1,13 +1,35 @@
 import { useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchProducts } from "../../redux/productsSlice";
+import { postContent } from "../../js/postContent";
+import { productsUrl } from "../../js/endpoints";
 import "./ProductCreator.css";
 
 export default function ProductCreator() {
     const [ingredients, setIngredients] = useState([]);
     const [allergens, setAllergens] = useState([]);
-    const ingredientRef = useRef(), allergenRef = useRef();
+    const nameRef = useRef(), typeRef = useRef(), imageRef = useRef(), ingredientRef = useRef(), allergenRef = useRef(), priceRef = useRef(), amountRef = useRef();
+    const dispatch = useDispatch(), navigate = useNavigate();
 
     const handleFormSubmit = (e) => {
         e.preventDefault();
+
+        if(ingredients.length < 1) return;
+
+        const product = {
+            name: nameRef.current.value,
+            type: typeRef.current.value,
+            imageUrl: imageRef.current.value,
+            ingredients: ingredients,
+            alergens: allergens,
+            price: priceRef.current.value,
+            amount: amountRef.current.value
+        }
+
+        postContent(product, productsUrl)
+            .then(() => dispatch(fetchProducts()))
+            .then(() => navigate("/admin"));
     }
 
     return (
@@ -16,15 +38,15 @@ export default function ProductCreator() {
             <form onSubmit={handleFormSubmit} className="creator-form">
                 <div className="creator-div">
                     <label htmlFor="name">Name: </label>
-                    <input type="text" name="name" placeholder="Ginger Berry Pie" required/>
+                    <input type="text" name="name" placeholder="Ginger Berry Pie" required ref={nameRef}/>
                 </div>
                 <div className="creator-div">
                     <label htmlFor="type">Type: </label>
-                    <input type="text" name="type" placeholder="dessert" required/>
+                    <input type="text" name="type" placeholder="dessert" required ref={typeRef}/>
                 </div>
                 <div className="creator-div">
                     <label htmlFor="imageUrl">Image: </label>
-                    <input type="text" name="imageUrl" placeholder="https://imageUrl" required/>
+                    <input type="text" name="imageUrl" placeholder="https://imageUrl" required ref={imageRef}/>
                 </div>
                 <div className="list-div">
                     <div className="item-list">
@@ -95,11 +117,11 @@ export default function ProductCreator() {
                 </div>
                 <div className="creator-div">
                     <label htmlFor="price">Price: </label>
-                    <input type="number" name="price" placeholder="12.35" required/>
+                    <input type="number" name="price" placeholder="12.35" required ref={priceRef}/>
                 </div>
                 <div className="creator-div">
                     <label htmlFor="amount">Amount: </label>
-                    <input type="number" name="amount" placeholder="2" required/>
+                    <input type="number" name="amount" placeholder="2" required ref={amountRef}/>
                 </div>
                 <div className="creator-submit">
                     <button type="submit" className="base-btn">Submit</button>
