86e6ab0f73e5d96e2b903c3e9558385b597ef58c
astrahalora <astrahalora@gmail.com>
2024-03-27 15:16:47 +0200

    Disable addtocart when improper amount left

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,8 +1,19 @@
+import { useSelector } from "react-redux";
 import cart from "../../img/shopping_cart.png";
 import unavailableCart from "../../img/shopping_cart_unavailable.png";
 import "./Product.css";
 
 export default function Product({ product, showDetails, addToCart }) {
+    const productsInCart = useSelector(state => state.cart.products);
+
+    const productAmountGreaterThanProductQuantity = () => {
+        const cartProduct = productsInCart.filter(item => item.name === product[0].name);
+        if(cartProduct[0]) {
+            return product[0].amount > cartProduct[0].quantity;
+        }
+        return true;
+    }
+
     return (
         <div className="product">
             <div>
@@ -15,7 +26,7 @@ export default function Product({ product, showDetails, addToCart }) {
                     <p className="price">
                         {product[0].amount > 0 ? `$` + product[0].price.toFixed(2) : "Unavailable"}
                     </p>
-                    {product[0].amount > 0 ? (
+                    {product[0].amount > 0 && productAmountGreaterThanProductQuantity() ? (
                         <img src={cart} alt="Cart" className="buy-cart" onClick={addToCart} />
                     ) : (
                         <img src={unavailableCart} alt="Cart" className="unavailable-cart" />
